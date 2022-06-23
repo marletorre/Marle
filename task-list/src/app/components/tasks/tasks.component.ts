c7486e46b6c5f17ba76cb9f348992affd97f3e1e
Marlene Torrealba <marlene.torrealbaf@gmail.com>
2022-06-23 10:58:59 -0300

    error

--- task-list/src/app/components/tasks/tasks.component.ts
+++ task-list/src/app/components/tasks/tasks.component.ts
@@ -16,4 +16,23 @@ export class TasksComponent implements OnInit {
   this.taskService.getTasks().subscribe((tasks:any)=>(this.tasks=tasks));
   }
 
+  deleteTask(task:Task){
+    this.taskService.deleteTask(task)
+    .subscribe(
+      ()=>
+        this.tasks=this.tasks.filter( (t)=> {
+          return t.id !== task.id
+        })
+      )
+  }
+
+  toggleReminder(task:Task){
+    task.reminder=!task.reminder
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+
+  addTask(task:Task){
+    this.taskService.addTask(task).subscribe((task: Task)=>(this.tasks.push(task)));
+      
+  }
 }
